0f193da348cf9f88b424d48742552ce91d02418f
Gwenaël Ayllón <gwenael.ayllon.formasup82@gmail.com>
2026-06-29 22:28:14 +0200

    fix scroll iOS menu2

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -16,18 +16,26 @@ export default function Nav() {
 
   useEffect(() => {
     if (open) {
+      const scrollY = window.scrollY;
       document.body.style.overflow = "hidden";
       document.body.style.position = "fixed";
+      document.body.style.top = `-${scrollY}px`;
       document.body.style.width = "100%";
     } else {
+      const scrollY = document.body.style.top;
       document.body.style.overflow = "";
       document.body.style.position = "";
+      document.body.style.top = "";
       document.body.style.width = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
     }
     return () => {
+      const scrollY = document.body.style.top;
       document.body.style.overflow = "";
       document.body.style.position = "";
+      document.body.style.top = "";
       document.body.style.width = "";
+      window.scrollTo(0, parseInt(scrollY || "0") * -1);
     };
   }, [open]);
 
