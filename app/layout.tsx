1a755eab470482b30da00b43a2fd1251dbda3f83
Gwenaël Ayllón <gwenael.ayllon.formasup82@gmail.com>
2026-06-29 19:53:23 +0200

    fix viewport mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -22,6 +22,11 @@ export const metadata: Metadata = {
     locale: "fr_FR",
     type: "website",
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
